f336f07971b945d1f3e045087c6df3399e74bd14
Le Nguyen Thai Duong <lntduongit@gmail.com>
2023-09-25 22:11:45 +0700

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,34 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
+
+import Navbar from '@/components/navbar/Navbar';
+import Footer from '@/components/footer/Footer';
+import ThemeContext, { ThemeContextProvider } from '@/context/ThemeContext';
+import ThemeProvider from '@/providers/ThemeProvider';
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
-}
+	title: 'Blog App',
+	description: 'The best blog app!',
+};
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang='en'>
+			<body className={inter.className} suppressHydrationWarning={true}>
+				<ThemeContextProvider>
+					<ThemeProvider>
+						<div className='container'>
+							<div className='wrapper'>
+								<Navbar />
+								{children}
+								<Footer />
+							</div>
+						</div>
+					</ThemeProvider>
+				</ThemeContextProvider>
+			</body>
+		</html>
+	);
 }
